0c6cc6fe1545e8feb8aa59c03999556a089accc8
Jose Lopez <jose.lopez@sendgrid.com>
2013-09-24 23:30:37 -0700

    initial bootstrap commit

---
+++ setup.sh
@@ -0,0 +1,101 @@
+INSTALL_BOOTSTRAP=true
+INSTALL_SUBLIME=true
+INSTALL_HIPCHAT=true
+INSTALL_SPOTIFY=true
+INSTALL_RUBY=true
+INSTALL_VAGRANT=true
+
+
+if $INSTALL_BOOTSTRAP; then
+  echo '-- bootstrapping system'
+  sudo apt-get -y install curl git vim indicator-multiload hamster-indicator chromium-browser keepassx
+  sudo apt-get remove unity-lens-shopping
+fi
+
+if $INSTALL_SUBLIME; then
+  echo '-- installing sublime'
+  sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+  sudo apt-get update
+  sudo apt-get install sublime-text-installer
+
+  # install custom preferences
+  echo '
+// Settings in here override those in "Default/Preferences.sublime-settings",
+// and are overridden in turn by file type specific settings.
+{
+    // The number of spaces a tab is considered equal to
+    "tab_size": 2,
+
+    // Set to true to insert spaces when tab is pressed
+    "translate_tabs_to_spaces": true,
+
+    // Set to true to removing trailing white space on save
+    "trim_trailing_white_space_on_save": true,
+}' > ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+
+  echo '
+// Settings in here override those in "Default/Preferences.sublime-settings",
+// and are overridden in turn by file type specific settings.
+{
+    // The number of spaces a tab is considered equal to
+    "tab_size": 4,
+}' > ~/.config/sublime-text-3/Packages/User/Python.sublime-settings
+
+  wget https://sublime.wbond.net/Package%20Control.sublime-package
+  mv Package\ Control.sublime-package ~/.config/sublime-text-3/Installed\ Packages/.
+
+  git clone git://github.com/jlope001-/Flake8Lint.git "Python Flake8 Lint"
+  rm -rf ~/.config/sublime-text-3/Packages/Python\ Flake8\ Lint
+  mv Python\ Flake8\ Lint ~/.config/sublime-text-3/Packages/.
+fi
+
+if $INSTALL_HIPCHAT; then
+  if [ -f  /etc/apt/sources.list.d/atlassian-hipchat.list ]; then
+    echo '-- hipchat already installed'
+  else
+    echo '-- installing hipchat'
+    echo "deb http://downloads.hipchat.com/linux/apt stable main" | sudo tee -a /etc/apt/sources.list.d/atlassian-hipchat.list
+    sudo wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
+    sudo apt-get update
+    sudo apt-get -y install hipchat
+  fi
+fi
+
+if $INSTALL_SPOTIFY; then
+  echo '-- installing spotify'
+  SPOTIFY=$(cat /etc/apt/sources.list | grep spotify | wc -l)
+  if [ ! $SPOTIFY -eq 1 ];
+  then
+    echo '-- not installing spotify'
+  else
+    echo '-- installing spotify'
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee -a /etc/apt/sources.list
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+    sudo apt-get update
+    sudo apt-get install -y spotify-client
+  fi
+fi
+
+if $INSTALL_RUBY; then
+  echo '-- installing ruby'
+  \curl -L https://get.rvm.io | bash -s stable --ruby
+  source /home/jlope001/.rvm/scripts/rvm
+  rvm install 1.9.3
+  rvm --default use 1.9.3
+  RVM_SOURCE=$(cat ~/.bashrc | grep '.rvm/scripts/rvm' | wc -l)
+  echo $RVM_SOURCE
+  if [ $RVM_SOURCE -ge 1 ];
+  then
+    echo '-- not adding source entry'
+  else
+    echo '-- adding source entry'
+    echo "source $HOME/.rvm/scripts/rvm" >> ~/.bashrc
+  fi
+fi
+
+if $INSTALL_VAGRANT; then
+  echo '-- installing vagrant'
+  wget http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb
+  sudo dpkg -i vagrant_1.2.2_x86_64.deb
+  rm vagrant_1.2.2_x86_64.deb
+fi
